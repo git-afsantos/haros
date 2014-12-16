c91b72c866683f08a901a4f35f5d0c0c4cb73adf
git-afsantos <contact.andre.santos@gmail.com>
2014-12-16 09:41:18 +0000

    Print source files and number of violations detected. Updated .gitignore to ignore .pyc files and credentials files.

--- scrapper/compliance_parser.py
+++ scrapper/compliance_parser.py
@@ -1,3 +1,4 @@
+import os
 import xml.etree.ElementTree as ET
 
 
@@ -8,11 +9,35 @@ def read_compliance_from_xml(cmp_file):
     files = {}
     for warning in root:
         src_file = warning.find("absFile").text
-        src_file = src_file[(src_file.find("/ros/repos/") + 11):]
-        check = warning.find("checkName").text
-        if not src_file in files:
-            files[src_file] = {}
-        rules = files[src_file]
-        rules[check] = rules.get(check, 0) + 1
+        prefix = src_file.find("/ros/repos/")
+        # Ignore system and library files.
+        if prefix > -1:
+            src_file = src_file[(prefix + 11):]
+            check = warning.find("checkName").text
+            if not src_file in files:
+                files[src_file] = {}
+            rules = files[src_file]
+            rules[check] = rules.get(check, 0) + 1
     return files
+
+
+def read_compliance_files(path_to_dir):
+    results = {}
+    for root, subdirs, xml_files in os.walk(path_to_dir):
+        for xml_file in xml_files:
+            cmp_dict = read_compliance_from_xml(os.path.join(root, xml_file))
+            for src_file, rules in cmp_dict.iteritems():
+                for rule, value in rules.iteritems():
+                    results[src_file] = results.get(src_file, 0) + value
+    return results
+
+
+
+
+if __name__ == "__main__":
+    src_root    = os.path.join(os.path.expanduser("~"), "ros", "goanna")
+    print "Reading XML files..."
+    files       = read_compliance_files(src_root)
+    for f, v in files.iteritems():
+        print "\t[{0}]:\t{1}".format(files[f], f)
 
