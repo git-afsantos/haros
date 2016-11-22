330ea843c3463c455ba98c169af92680a9d7afb3
git-afsantos <contact.andre.santos@gmail.com>
2016-11-22 23:01:32 +0000

    bug fix

--- ana2/plugins/dummy/plugin.py
+++ ana2/plugins/dummy/plugin.py
@@ -2,4 +2,4 @@ print "[dummy] loading plugin"
 
 def file_analysis(iface, scope):
     print "Analysing file", scope.id
-    iface.report_violation("roscpp_4.3", "Dummy violation for " scope.id, line = 1)
+    iface.report_violation("roscpp_4.3", "Dummy violation for " + scope.id, line = 1)
