bee5ff1d2b013c8adf39a82399029487bdf61474
Andre Santos <contact.andre.santos@gmail.com>
2017-11-02 17:19:59 +0000

    [WIP] converting commands into classes; init and viz complete

--- haros/haros.py
+++ haros/haros.py
@@ -111,6 +111,7 @@ class HarosLauncher(object):
     """
 
     HAROS_DIR       = os.path.join(os.path.expanduser("~"), ".haros")
+    DEFAULT_INDEX   = os.path.join(HAROS_DIR, "index.yaml")
     LOG_PATH        = os.path.join(HAROS_DIR, "log.txt")
     REPOSITORY_DIR  = os.path.join(HAROS_DIR, "repositories")
     EXPORT_DIR      = os.path.join(HAROS_DIR, "export")
@@ -120,20 +121,11 @@ class HarosLauncher(object):
     DEFAULT_PROJECT = os.path.join(PROJECTS_DIR, "default")
     DB_PATH         = os.path.join(DEFAULT_PROJECT, "haros.db")
     ANALYSIS_PATH   = os.path.join(DEFAULT_PROJECT, "analysis.db")
-    PLUGIN_REPOSITORY = "https://github.com/git-afsantos/haros_plugins.git"
-
-    DIR_STRUCTURE = {
-        "index.yaml": "%YAML 1.1\n---\npackages: []\n",
-        "plugins": {},
-        "repositories": {},
-        "export": {},
-        "projects": {}
-        # viz is generated on viz.install
-    }
 
     def __init__(self, run_from_source = False):
         self.log = logging.getLogger()
         self.run_from_source = run_from_source
+        self.initialised = False
 
     def launch(self, argv = None):
         args = self.parse_arguments(argv)
@@ -144,21 +136,39 @@ class HarosLauncher(object):
             logging.basicConfig(level = logging.WARNING)
         original_path = os.getcwd()
         try:
+            if not os.path.isdir(HAROS_DIR):
+                print "[HAROS] It seems this is a first run."
+                self.command_init(args)
             if args.dir:
                 os.chdir(args.dir)
             self.log.info("Executing selected command.")
-            args.func(args)
+            return args.command(args)
         except RuntimeError as err:
             self.log.error(str(err))
-            raise err
+            return False
         finally:
             os.chdir(original_path)
+
+    def command_init(self, args):
+        if not self.initialised:
+            init = HarosInitRunner(self.HAROS_DIR, log = self.log,
+                                   run_from_source = self.run_from_source)
+            if not init.run():
+                return False
+            self.initialised = True
+        return True
 
     def command_full(self, args):
         pass
 
     def command_analyse(self, args):
-        pass
+        if not os.path.isdir(args.target_dir):
+            raise ValueError("Not a directory: " + args.target_dir)
+        if not os.path.isfile(args.pkg_filter):
+            raise ValueError("Not a file: " + args.pkg_filter)
+        analyse = HarosAnalyseRunner(args.pkg_filter, args.target_dir, log = self.log,
+                                     run_from_source = self.run_from_source)
+        return analyse.run()
 
     def command_export(self, args):
         pass
@@ -186,6 +196,7 @@ class HarosLauncher(object):
         parser.set_defaults(command = self.command_init)
 
     def _full_parser(self, parser):
+        # TODO try to remove these dumb `dest`
         parser.add_argument("-r", "--repositories",
                             dest = "use_repos", action = "store_true",
                             help = "use repositories")
@@ -194,6 +205,7 @@ class HarosLauncher(object):
                             help = ("visualisation host "
                                     "(default: \"localhost:8080\")"))
         parser.add_argument("-p", "--package-index", dest = "pkg_filter",
+                            default = self.DEFAULT_INDEX,
                             help = ("package index file (default: "
                                     "packages below current dir)"))
         parser.add_argument("-t", "--target-dir", default = self.VIZ_DIR,
@@ -211,6 +223,7 @@ class HarosLauncher(object):
         parser.add_argument("-r", "--repositories", dest = "use_repos",
                             action = "store_true", help = "use repositories")
         parser.add_argument("-p", "--package-index", dest = "pkg_filter",
+                            default = self.DEFAULT_INDEX,
                             help = ("package index file (default: "
                                     "packages below current dir)"))
         parser.add_argument("-t", "--target-dir", default = self.VIZ_DIR,
@@ -251,31 +264,165 @@ class HarosRunner(object):
         self.run_from_source = run_from_source
 
     def run(self):
-        pass
+        return True
 
+    def _generate_dir(self, path, dir_dict):
+        """Recursively create a given directory structure."""
+        self.log.debug("HarosRunner._generate_dir %s %s", path, str(dir_dict))
+        for name, contents in dir_dict.iteritems():
+            new_path = os.path.join(path, name)
+            if isinstance(contents, basestring):
+                if os.path.exists(new_path) and not os.path.isfile(new_path):
+                    raise RuntimeError("Could not create file: " + new_path)
+                self.log.info("Creating %s", new_path)
+                with open(new_path, "w") as handle:
+                    handle.write(contents)
+            elif isinstance(contents, dict):
+                if os.path.exists(new_path) and not os.path.isdir(new_path):
+                    raise RuntimeError("Could not create dir: " + new_path)
+                self.log.info("Creating %s", new_path)
+                os.mkdir(new_path)
+                self._generate_dir(new_path, contents)
+
+
+
+class HarosInitRunner(HarosRunner):
+    PLUGIN_REPOSITORY = "https://github.com/git-afsantos/haros_plugins.git"
+
+    DIR_STRUCTURE = {
+        "index.yaml": "%YAML 1.1\n---\npackages: []\n",
+        "plugins": {},
+        "repositories": {},
+        "export": {},
+        "projects": {
+            "default": {}
+        }
+        # viz is generated on viz.install
+    }
+
+    def __init__(self, haros_dir, log = None, run_from_source = False):
+        HarosRunner.__init__(self, log = log, run_from_source = run_from_source)
+        self.root = haros_dir
+        self.plugin_dir = os.path.join(haros_dir, "plugins")
+        self.viz_dir = os.path.join(haros_dir, "viz")
+
+    def run(self):
+        print "[HAROS] Running initial setup operations..."
+        if os.path.exists(self.root) and not os.path.isdir(self.root):
+            raise RuntimeError(("Could not init; " + self.root
+                                + " already exists and is not a directory."))
+        if not os.path.exists(self.root):
+            self.log.info("Creating %s", self.root)
+            os.makedirs(self.root)
+        has_plugins = os.path.exists(self.plugin_dir)
+        self._generate_dir(self.root, self.DIR_STRUCTURE)
+        viz.install(self.viz_dir, self.run_from_source)
+        if has_plugins:
+            self.log.info("Updating plugin repository.")
+            wd = os.getcwd()
+            os.chdir(self.plugin_dir)
+            with open(os.devnull, "w") as devnull:
+                subprocess.check_call(["git", "rev-parse"], stdout = devnull,
+                                      stderr = subprocess.STDOUT)
+                self.log.debug("Executing git pull at %s.", self.plugin_dir)
+                subprocess.check_call(["git", "pull"])
+            os.chdir(wd)
+        else:
+            self.log.info("Cloning plugin repository.")
+            subprocess.check_call(["git", "clone",
+                                   self.PLUGIN_REPOSITORY, self.plugin_dir])
+       return True
 
 
 class HarosFullRunner(HarosRunner):
-    pass
+    def __init__(self, target_dir, host_str, log = None, run_from_source = False):
+        HarosRunner.__init__(self, log = log, run_from_source = run_from_source)
+        self.root = target_dir
+        self.host = host_str
+
+    def run(self):
+        return HarosAnalyseRunner.run(self) and HarosVizRunner.run(self)
 
 
 class HarosAnalyseRunner(HarosRunner):
-    pass
+    def __init__(self, pkg_filter, target_dir, log = None, run_from_source = False):
+        HarosRunner.__init__(self, log = log, run_from_source = run_from_source)
+        self.projects = [pkg_filter]
+        self.target_dir = target_dir
+
+    def run(self):
+        print "[HAROS] Indexing source code..."
+        self.log.debug("Creating new data manager.")
+        dataman = DataManager()
+        self.log.debug("Package index file %s", self.projects[0])
+        dataman.index_source(self.projects[0], REPOSITORY_DIR, args.use_repos)
+        if not dataman.packages:
+            self.log.warning("There are no packages to analyse.")
+            return False
+        print "[HAROS] Loading common definitions..."
+        if args.source_runner:
+            path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   "definitions.yaml"))
+        else:
+            path = resource_filename(Requirement.parse("haros"),
+                                     "haros/definitions.yaml")
+        dataman.load_definitions(path)
+        print "[HAROS] Loading plugins..."
+        plugins = plugman.load_plugins(PLUGIN_DIR, args.whitelist, args.blacklist)
+        if not plugins:
+            self.log.warning("There are no analysis plugins.")
+            return False
+        for plugin in plugins:
+            dataman.extend_definitions(plugin.name, plugin.rules, plugin.metrics)
+        print "[HAROS] Running analysis..."
+        _empty_dir(EXPORT_DIR)
+        db_path = os.path.join(PROJECTS_DIR, dataman.project.name)
+        args.analysis_db = args.analysis_db or os.path.join(db_path, "analysis.db")
+        if os.path.isfile(args.analysis_db):
+            anaman = AnalysisManager.load_state(args.analysis_db)
+        else:
+            anaman = AnalysisManager()
+        temppath = tempfile.mkdtemp()
+        anaman.run_analysis_and_processing(temppath, plugins, dataman, EXPORT_DIR)
+        rmtree(temppath)
+        print "[HAROS] Saving analysis results..."
+        # TODO this needs refactoring
+        if not os.path.isdir(db_path):
+            self.log.info("Creating %s...", db_path)
+            os.mkdir(db_path)
+        dataman.save_state(os.path.join(db_path, "haros.db"))
+        anaman.save_state(args.analysis_db)
+        index_file = os.path.join(args.target_dir, "index.html")
+        args.export_viz = (args.target_dir != VIZ_DIR
+                           and not os.path.isfile(index_file))
+        command_export(args, dataman, anaman)
+    
+    
+    
+        return True
 
 
 class HarosExportRunner(HarosRunner):
-    pass
+    def __init__(self, target_dir, log = None, run_from_source = False):
+        HarosRunner.__init__(self, log = log, run_from_source = run_from_source)
+        self.target_dir = target_dir
+
+    def run(self):
+        return True
 
 
 class HarosVizRunner(HarosRunner):
-    def __init__(self, args):
+    def __init__(self, server_dir, host_str, log = None,
+                 run_from_source = False):
+        HarosRunner.__init__(self, log = log, run_from_source = run_from_source)
+        self.server_dir = server_dir
+        self.host = host_str
+
+    def run(self):
+        return viz.serve(self.server_dir, self.host)
 
 
 
-
-def _check_haros_directory():
-    if not os.path.isdir(HAROS_DIR):
-        raise RuntimeError("HAROS directory was not initialised.")
 
 def _empty_dir(dir_path):
     self.log.debug("Emptying directory %s", dir_path)
@@ -284,49 +431,6 @@ def _empty_dir(dir_path):
         if os.path.isfile(path):
             self.log.debug("Removing file %s", path)
             os.unlink(path)
-
-def command_init(args):
-    print "[HAROS] Creating directories..."
-    if os.path.exists(HAROS_DIR) and not os.path.isdir(HAROS_DIR):
-        raise RuntimeError("Could not init; " + HAROS_DIR \
-                           + " already exists and is not a directory.")
-    if not os.path.exists(HAROS_DIR):
-        self.log.info("Creating %s", HAROS_DIR)
-        os.makedirs(HAROS_DIR)
-        self.log.info("Creating %s", os.path.join(HAROS_DIR, "index.yaml"))
-        with open(os.path.join(HAROS_DIR, "index.yaml"), "w") as f:
-            f.write("%YAML 1.1\n---\npackages: []\n")
-    if not os.path.exists(REPOSITORY_DIR):
-        self.log.info("Creating %s", REPOSITORY_DIR)
-        os.mkdir(REPOSITORY_DIR)
-    if not os.path.exists(EXPORT_DIR):
-        self.log.info("Creating %s", EXPORT_DIR)
-        os.mkdir(EXPORT_DIR)
-    if not os.path.exists(PROJECTS_DIR):
-        self.log.info("Creating %s", PROJECTS_DIR)
-        os.mkdir(PROJECTS_DIR)
-        self.log.info("Creating default project dir")
-        os.mkdir(DEFAULT_PROJECT)
-    viz.install(VIZ_DIR, args.source_runner)
-    if not os.path.exists(PLUGIN_DIR):
-        self.log.info("Creating %s", PLUGIN_DIR)
-        os.mkdir(PLUGIN_DIR)
-        self.log.info("Cloning plugin repository.")
-        subprocess.check_call(["git", "clone", PLUGIN_REPOSITORY, PLUGIN_DIR])
-    else:
-        self.log.info("Updating plugin repository.")
-        wd = os.getcwd()
-        os.chdir(PLUGIN_DIR)
-        if subprocess.call(["git", "branch"], stderr = subprocess.STDOUT,
-                           stdout = open(os.devnull, 'w')) == 0:
-            self.log.info("%s is a git repository. Executing git pull.", PLUGIN_DIR)
-            subprocess.check_call(["git", "pull"])
-        os.chdir(wd)
-
-
-def command_full(args):
-    return command_analyse(args) and command_viz(args)
-
 
 def command_analyse(args):
     _check_haros_directory()
@@ -466,11 +570,6 @@ def command_export(args, dataman = None, anaman = None):
         self.log.debug("Copying analysis DB from %s to %s", ana_file, ana_path)
         copyfile(ana_file, ana_path)
     return True
-
-
-def command_viz(args):
-    _check_haros_directory()
-    return viz.serve(args.target_dir, args.host)
 
 
 def main(argv = None, source_runner = False):
