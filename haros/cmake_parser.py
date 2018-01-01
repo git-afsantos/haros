f0dbe2875f99b747e162b21f59d284fe22ee9dc1
git-afsantos <contact.andre.santos@gmail.com>
2018-01-01 12:09:27 +0000

    [WIP] added comment with indications for default workspace

--- haros/cmake_parser.py
+++ haros/cmake_parser.py
@@ -810,3 +810,31 @@ class RosCMakeParser(object):
         self.libraries = {}
         self.executables = {}
         self.subdirectories = []
+
+
+"""
+function roscd {
+    local rosvals
+    if [[ $1 = "--help" ]] || [[ $# -gt 1 ]]; then
+        echo -e "usage: roscd package\n\nJump to target package."
+        return 0
+    fi
+    if [ -z $1 ]; then
+      if [ ! -z $ROS_WORKSPACE ]; then
+        cd ${ROS_WORKSPACE}
+        return 0
+      fi
+      if [ ! -z $CMAKE_PREFIX_PATH ]; then
+        IFS=":" read -a workspaces <<< "$CMAKE_PREFIX_PATH"
+        for ws in "${workspaces[@]}"; do
+          if [ -f $ws/.catkin ]; then
+            cd ${ws}
+            return 0
+          fi
+        done
+      fi
+      echo -e "Neither ROS_WORKSPACE is set nor a catkin workspace is listed in CMAKE_PREFIX_PATH.  Please set ROS_WORKSPACE or source a catkin workspace to use roscd with no arguments."
+      return 1
+    fi
+}
+"""
