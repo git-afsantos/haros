ba496d1f24ec0439bcd3e52a537aa02851250d9f
Davide Laezza <truzzialrogo@gmx.com>
2018-12-16 20:12:11 +0100

    Using magic to determine file type - more reliable than extensions

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     install_requires = [
         "pyyaml",
         "rospkg",
+        'python-magic',
         "bonsai-code>=0.4.10,<1.0.0",
         "haros-plugins>=1.0.3,<2.0.0"
     ],
