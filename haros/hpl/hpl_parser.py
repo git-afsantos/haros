49e78c5af444bd0a7b6ba49b95eb5a14f0f06836
André Santos <contact.andre.santos@gmail.com>
2020-04-01 19:45:40 +0100

    WIP parser for new language version

--- haros/hpl/hpl_parser.py
+++ haros/hpl/hpl_parser.py
@@ -27,6 +27,7 @@
 
 from builtins import range # Python 2 and 3: forward-compatible
 from collections import namedtuple
+import math
 
 from lark import Lark, Transformer
 from lark.exceptions import UnexpectedCharacters, UnexpectedToken
@@ -34,7 +35,8 @@ from lark.lexer import Token
 import logging
 
 from .hpl_ast import (
-    HplProperty, HplScope, HplObservable, HplEvent, HplChainDisjunction,
+    HplAstObject, HplProperty, HplScope, HplObservable, HplEvent,
+    HplChainDisjunction,
     HplEventChain, HplMessageFilter, HplFieldCondition, HplValue, HplLiteral,
     HplSet, HplRange, HplFieldReference, HplAssumption, HplSanityError
 )
@@ -45,59 +47,81 @@ from .hpl_ast import (
 ###############################################################################
 
 INF = float("inf")
+NAN = float("nan")
 
 
 ###############################################################################
 # Grammar
 ###############################################################################
 
-MSG_FILTER_GRAMMAR = r"""
-msg_filter: "{" _msg_condition ("," _msg_condition)* "}"
+PREDICATE_GRAMMAR = r"""
+_predicate: "{" condition "}"
 
-_msg_condition: field_condition
-              | field_attr_condition
+condition: [condition IF_OPERATOR] disjunction
 
-field_condition: own_msg_field_expr _value_condition
+disjunction: [disjunction OR_OPERATOR] conjunction
 
-field_attr_condition: own_field_attr_expr _attr_value_condition
+conjunction: [conjunction AND_OPERATOR] _logic_expr
 
-own_msg_field_expr: FIELD_REF
+_logic_expr: atomic_condition
+           | "(" condition ")"
+           | negation
+           | _quantification
 
-own_field_attr_expr: FIELD_ATTR
+negation: NOT_OPERATOR _logic_expr
 
-_value_condition: EQ_OPERATOR _value
-                | COMP_OPERATOR _number_value
-                | IN_OPERATOR _set_value
-                | nin_operator _set_value
+_quantification: universal | existential
 
-_attr_value_condition: EQ_OPERATOR int_literal
-                     | COMP_OPERATOR int_literal
+universal: ALL_OPERATOR CNAME ":" _logic_expr
 
-nin_operator: NOT_OPERATOR IN_OPERATOR
+existential: SOME_OPERATOR CNAME ":" _logic_expr
 
-_value: string
-      | signed_number
-      | msg_field_expr
+atomic_condition: _value BINARY_OPERATOR _value
 
-_number_value: signed_number
-             | msg_field_expr
+_value: boolean
+      | string
+      | number_expr
+      | _set_value
+      | function_call
 
-_set_value: enum_literal
-          | range_literal
+function_call: BUILTIN_FUNCTION "(" _value ")"
 
-enum_literal: "[" _enum_value ("," _enum_value)* "]"
+number_expr: [number_expr ADD_OPERATOR] number_term
 
-_enum_value: string
-           | signed_number
-           | msg_field_expr
+number_term: [number_term MULT_OPERATOR] number_factor
+
+number_factor: [number_factor POWER_OPERATOR] _number_value
+
+_number_value: number_constant
+             | signed_number
+             | _msg_field
+             | variable
+             | "(" number_expr ")"
+             | negative_number
+
+number_constant: CONSTANT
+
+negative_number: "-" _number_value
+
+_set_value: "[" _set_contents "]"
+
+_set_contents: enum_literal
+             | range_literal
+
+enum_literal: _value ("," _value)*
 
 range_literal: range_limit "to" range_limit
 
-range_limit: signed_number VALUE_EXCLUSION?
-           | msg_field_expr VALUE_EXCLUSION?
+range_limit: number_expr VALUE_EXCLUSION?
 
-msg_field_expr: FIELD_REF
-              | ALIAS_REF "." FIELD_REF
+variable: VAR_REF
+
+_msg_field: own_msg_field
+          | ext_msg_field
+
+own_msg_field: FIELD_REF
+
+ext_msg_field: EXT_FIELD_REF
 
 ros_name: ROS_NAME
 
@@ -105,101 +129,96 @@ int_literal: INT
 string: ESCAPED_STRING
 number: NUMBER
 signed_number: SIGNED_NUMBER
+boolean: TRUE | FALSE
 
+TRUE = "True"
+FALSE = "False"
+
+BINARY_OPERATOR: EQ_OPERATOR | COMP_OPERATOR | IN_OPERATOR
 EQ_OPERATOR: "=" | "!="
 COMP_OPERATOR: "<" "="?
              | ">" "="?
 IN_OPERATOR: "in"
 NOT_OPERATOR: "not"
+IF_OPERATOR: "implies" | "if" | "iff"
+OR_OPERATOR: "or"
+AND_OPERATOR: "and"
+ALL_OPERATOR: "forall"
+SOME_OPERATOR: "exists"
 
-VALUE_EXCLUSION: "(exc)"
+CONSTANT: "PI" | "INF" | "NAN"
+ADD_OPERATOR: "+" | "-"
+MULT_OPERATOR: "*" | "/"
+POWER_OPERATOR: "**"
+
+VALUE_EXCLUSION: "!"
 
 ROS_NAME: /[\/~]?[a-zA-Z][0-9a-zA-Z_]*(\/[a-zA-Z][0-9a-zA-Z_]*)*/
 
-ALIAS_REF: "$" CNAME
-
-FIELD_ATTR: FIELD_REF "#" KNOWN_ATTR
+VAR_REF: "@" CNAME
 
 FIELD_REF: MSG_FIELD ("." MSG_FIELD)*
 
-MSG_FIELD: CNAME ["[" (INT | ARRAY_OPERATOR) "]"]
+MSG_FIELD: CNAME ARRAY_ACCESS?
 
-ARRAY_OPERATOR: "*" ["\\" INT ("," INT)*]
+ARRAY_ACCESS: "[" (INT | VAR_REF) "]"
 
-KNOWN_ATTR: "length"
+EXT_FIELD_REF: VAR_REF "." FIELD_REF
+
+BUILTIN_FUNCTION: "len" | "abs" | "bool" | "int" | "float" | "str"
+                | "max" | "min" | "sum" | "prod"
 """
 
 
 PROPERTY_GRAMMAR = (r"""
-hpl_property: _scope ":" _observable
+hpl_property: _scope ":" _pattern
 
 _scope: global_scope
-      | _window_scope
+      | after_until
+      | until
 
 global_scope: "globally"
 
-_window_scope: after_event_until_event
-             | within_time_after_event
+after_until: "after" activator ["until" terminator]
 
-after_event_until_event: "after" activator_event ["until" terminator_event]
+until: "until" terminator
 
-within_time_after_event: _time_bound "after" activator_event
+activator: event
 
-activator_event: LAUNCH
-               | _top_level_event
+terminator: event
 
-terminator_event: SHUTDOWN
-                | _top_level_event
+_pattern: existence
+        | absence
+        | response
+        | prevention
+        | requirement
 
-_observable: observe_existence
-           | observe_absence
-           | observe_response
-           | observe_requirement
+existence: "some" event _time_bound?
 
-observe_existence: "some" _top_level_event
+absence: "no" event _time_bound?
 
-observe_absence: "no" _top_level_event
+response: event "causes" event _time_bound?
 
-observe_response: _top_level_event "causes" _time_bound? _top_level_event
+prevention: event "forbids" event _time_bound?
 
-observe_requirement: _top_level_event "requires" _time_bound? _top_level_event
-
-_top_level_event: chain_disjunction
-
-chain_disjunction: event_chain ("||" event_chain)*
-
-event_chain: event (chain_connector event)*
-
-chain_connector: ";" _chain_time_gap?
-
-_chain_time_gap: "[" _time_interval "]"
+requirement: event "requires" event _time_bound?
 
 _time_bound: "within" time_amount
 
-_time_interval: duration_without_delay
-              | delay_and_duration
+event: message _predicate?
 
-duration_without_delay: time_amount
-
-delay_and_duration: time_amount "to" time_amount
-
-event: ros_name event_attributes
-
-event_attributes: msg_filter? alias?
+message: ros_name _alias?
 
 time_amount: NUMBER TIME_UNIT
 
 frequency: NUMBER FREQ_UNIT
 
-alias: "as" CNAME
+_alias: "as" CNAME
 """
-+ MSG_FILTER_GRAMMAR
++ PREDICATE_GRAMMAR
 + r"""
 TIME_UNIT: "s" | "ms"
 FREQ_UNIT: "hz"
-
-LAUNCH: "launch"
-SHUTDOWN: "shutdown"
 
 %import common.CNAME
 %import common.INT
@@ -212,9 +231,9 @@ SHUTDOWN: "shutdown"
 
 
 ASSUMPTION_GRAMMAR = (r"""
-hpl_assumption: ros_name msg_filter
+hpl_assumption: ros_name _predicate
 """
-+ MSG_FILTER_GRAMMAR
++ PREDICATE_GRAMMAR
 + r"""
 %import common.CNAME
 %import common.INT
@@ -231,13 +250,11 @@ hpl_assumption: ros_name msg_filter
 ###############################################################################
 
 class PropertyTransformer(Transformer):
-    def hpl_assumption(self, (ros_name, msg_filter)):
-        assert len(msg_filter) == 2 and msg_filter[0] == "msg_filter"
-        msg_filter = msg_filter[1]
-        return HplAssumption(ros_name, msg_filter)
+    def hpl_assumption(self, (ros_name, predicate)):
+        return HplAssumption(ros_name, predicate)
 
-    def hpl_property(self, (scope, observable)):
-        hpl_property = HplProperty(scope, observable)
+    def hpl_property(self, (scope, pattern)):
+        hpl_property = HplProperty(scope, pattern)
         hpl_property.sanity_check()
         return hpl_property
 
@@ -245,186 +262,141 @@ class PropertyTransformer(Transformer):
         assert not children
         return HplScope.globally()
 
-    def after_event_until_event(self, children):
-        assert len(children) >= 1 and len(children) <= 2
-        activator = children[0]
-        terminator = None
+    def after_until(self, children):
+        assert len(children) == 1 or len(children) == 2
+        p = children[0]
         if len(children) == 2:
-            terminator = children[1]
-        return HplScope.after(activator, terminator=terminator)
+            return HplScope.after(p, terminator=children[1])
+        return HplScope.after(p)
 
-    def within_time_after_event(self, (timeout, activator)):
-        return HplScope.after(activator, timeout=timeout)
+    def until(self, (event,)):
+        return HplScope.until(event)
 
-    def activator_event(self, (event,)):
-        if event == "launch":
-            return None
+    def activator(self, (event,)):
         return event
 
-    def terminator_event(self, (event,)):
-        if event == "shutdown":
-            return None
+    def terminator(self, (event,)):
         return event
 
-    def observe_existence(self, (event,)):
-        return HplObservable.existence(event)
+    def existence(self, children):
+        assert len(children) == 1 or len(children) == 2
+        b = children[0]
+        max_time = INF if len(children) == 1 else children[1]
+        return HplPattern.existence(b, max_time=max_time)
 
-    def observe_absence(self, (event,)):
-        return HplObservable.absence(event)
+    def absence(self, children):
+        assert len(children) == 1 or len(children) == 2
+        b = children[0]
+        max_time = INF if len(children) == 1 else children[1]
+        return HplPattern.absence(b, max_time=max_time)
 
-    def observe_response(self, children):
-        assert len(children) >= 2
-        event = children[0]
-        response = children[-1]
-        max_time = INF
+    def response(self, children):
+        assert len(children) == 2 or len(children) == 3
+        a = children[0]
+        b = children[1]
+        max_time = INF if len(children) == 2 else children[2]
+        return HplPattern.response(a, b, max_time=max_time)
+
+    def prevention(self, children):
+        assert len(children) == 2 or len(children) == 3
+        a = children[0]
+        b = children[1]
+        max_time = INF if len(children) == 2 else children[2]
+        return HplPattern.prevention(a, b, max_time=max_time)
+
+    def requirement(self, children):
+        assert len(children) == 2 or len(children) == 3
+        b = children[0]
+        a = children[1]
+        max_time = INF if len(children) == 2 else children[2]
+        return HplPattern.requirement(b, a, max_time=max_time)
+
+    def event(self, children):
+        assert len(children) == 1 or len(children) == 2
+        ros_name, alias = children[0]
+        phi = None if len(children) == 1 else children[1]
+        return HplEvent(ros_name, alias=alias, predicate=phi)
+
+    def message(self, children):
+        alias = None if len(children) == 1 else children[1]
+        return (children[0], alias)
+
+    def condition(self, children):
+        return self._left_recursive_binop(children)
+
+    def disjunction(self, children):
+        return self._left_recursive_binop(children)
+
+    def conjunction(self, children):
+        return self._left_recursive_binop(children)
+
+    def negation(self, (op, phi)):
+        return HplUnaryOperator(op, phi)
+
+    def universal(self, (qt, var, phi)):
+        return HplQuantifier(qt, var, phi)
+
+    def existential(self, (qt, var, phi)):
+        return HplQuantifier(qt, var, phi)
+
+    def atomic_condition(self, (lhs, op, rhs)):
+        return HplBinaryOperator(op, lhs, rhs)
+
+    def function_call(self, (fun, args)):
+        return HplFunctionCall(fun, args)
+
+    def number_expr(self, children):
+        return self._left_recursive_binop(children)
+
+    def number_term(self, children):
+        return self._left_recursive_binop(children)
+
+    def number_factor(self, children):
+        return self._left_recursive_binop(children)
+
+    def _left_recursive_binop(self, children):
+        assert len(children) == 1 or len(children) == 3
         if len(children) == 3:
-            max_time = children[-2]
-        return HplObservable.response(event, response, max_time=max_time)
+            op = children[1]
+            lhs = children[0]
+            rhs = children[2]
+            return HplBinaryOperator(op, lhs, rhs)
+        return children[0] # len(children) == 1
 
-    def observe_requirement(self, children):
-        assert len(children) >= 2
-        event = children[0]
-        requirement = children[-1]
-        max_time = INF
-        if len(children) == 3:
-            max_time = children[-2]
-        return HplObservable.requirement(event, requirement, max_time=max_time)
+    _CONSTANTS = {
+        "PI": math.pi,
+        "INF": INF,
+        "NAN": NAN
+    }
 
-    def chain_disjunction(self, children):
-        assert len(children) >= 1
-        return HplChainDisjunction(children)
+    def number_constant(self, (c,)):
+        return HplLiteral(c, self._CONSTANTS[c])
 
-    def event_chain(self, children):
-        assert len(children) >= 1
-        end = len(children)
-        duration = INF
-        if end % 2 == 0:
-            duration = children[-1]
-            end = len(children) - 1
-        events = [children[0]]
-        for i in range(1, end, 2):
-            connector = children[i]
-            event = children[i + 1]
-            event.delay = connector[0]
-            event.duration = connector[1]
-            events.append(event)
-        return HplEventChain(events, duration=duration)
-
-    def chain_connector(self, children):
-        assert len(children) <= 1
-        if not children:
-            return (0.0, INF)
-        return children[0]
-
-    def duration_without_delay(self, (duration,)):
-        return (0.0, duration)
-
-    def delay_and_duration(self, (delay, duration)):
-        if delay >= duration:
-            raise ValueError("time interval with lower bound greater"
-                             " than the upper bound: " + str((delay, duration)))
-        return (delay, duration)
-
-    def event(self, (ros_name, attrs)):
-        return HplEvent.publish(ros_name, **attrs)
-
-    def event_attributes(self, children):
-        attrs = {}
-        for key, value in children:
-            attrs[key] = value
-        return attrs
-
-    def alias(self, (token,)):
-        return ("alias", token)
-
-    def msg_filter(self, conditions):
-        f_cond = []
-        len_cond = []
-        for condition, attr in conditions:
-            assert isinstance(condition, HplFieldCondition)
-            if attr is None:
-                f_cond.append(condition)
-            else:
-                assert attr == "length"
-                len_cond.append(condition)
-        return ("msg_filter", HplMessageFilter(f_cond, len_conditions=len_cond))
-
-    _EQ_OPERATORS = ("=", "!=")
-    _COMP_OPERATORS = ("<", "<=", ">", ">=")
-    _COMP_VALUE_TYPES = (int, float)
-    _IN_OPERATORS = ("in", "not in")
-
-    def field_condition(self, (field, operator, value)):
-        assert isinstance(field, HplFieldReference)
-        assert isinstance(value, HplValue)
-        if operator in self._COMP_OPERATORS:
-            assert value.is_reference or (value.is_literal
-                and isinstance(value.value, self._COMP_VALUE_TYPES))
-        elif operator in self._IN_OPERATORS:
-            assert value.is_set or value.is_range
-        return HplFieldCondition(field, operator, value), None
-
-    def field_attr_condition(self, ((field, attr), operator, value)):
-        assert isinstance(field, HplFieldReference)
-        assert isinstance(value, HplValue)
-        if operator in self._COMP_OPERATORS:
-            assert value.is_reference or (value.is_literal
-                and isinstance(value.value, self._COMP_VALUE_TYPES))
-        elif operator in self._IN_OPERATORS:
-            assert value.is_set or value.is_range
-        return HplFieldCondition(field, operator, value), attr
-
-    def nin_operator(self, (not_token, in_token)):
-        token = Token(not_token.type, "not in",
-            pos_in_stream=not_token.pos_in_stream,
-            line=not_token.line, column=not_token.column)
-        # these are not passed to the constructor,
-        # so we can use lark-parser<0.6.6
-        token.end_line = in_token.end_line
-        token.end_column = in_token.end_column
-        return token
-
-    def own_msg_field_expr(self, (token,)):
-        return HplFieldReference(token)
-
-    def own_field_attr_expr(self, (token,)):
-        assert "#" in token
-        field, attr = token.split("#")
-        field = Token(token.type, field, pos_in_stream=token.pos_in_stream,
-            line=token.line, column=token.column)
-        # these are not passed to the constructor,
-        # so we can use lark-parser<0.6.6
-        field.end_line = token.end_line
-        field.end_column = token.end_column - (len(attr) + 1)
-        attr = Token(token.type, attr, pos_in_stream=token.pos_in_stream,
-            line=token.line, column=(token.column + len(field) + 1))
-        # these are not passed to the constructor,
-        # so we can use lark-parser<0.6.6
-        attr.end_line = token.end_line
-        attr.end_column = token.end_column
-        field = HplFieldReference(field)
-        return field, attr
+    def negative_number(self, (n,)):
+        return HplUnaryOperator("-", n)
 
     def enum_literal(self, values):
         return HplSet(values)
 
-    def range_literal(self, limits):
-        assert len(limits) == 2
-        return HplRange(limits[0][0], limits[1][0],
-            exc_lower=limits[0][1], exc_upper=limits[1][1])
+    def range_literal(self, (lb, ub)):
+        return HplRange(lb[0], ub[0], exc_lower=lb[1], exc_upper=lb[1])
 
     def range_limit(self, children):
         exclude = len(children) == 2
-        assert not exclude or children[1] == "(exc)"
+        assert not exclude or children[1] == "!"
         return (children[0], exclude)
 
-    def msg_field_expr(self, tokens):
-        assert len(tokens) == 1 or len(tokens) == 2
-        if len(tokens) == 1:
-            return HplFieldReference(tokens[0])
-        else:
-            alias = tokens[0][1:] # remove lead "$"
-            return HplFieldReference(tokens[1], message=alias)
+    def variable(self, (var,)):
+        name = var[1:] # remove lead "@"
+        return HplVarReference(name)
+
+    def own_msg_field(self, (ref,)):
+        return HplFieldReference(ref)
+
+    def ext_msg_field(self, (ref,)):
+        alias, field = ref.split(".", 1)
+        name = alias[1:] # remove lead "@"
+        return HplFieldReference(field, message=name)
 
     def frequency(self, (n, unit)):
         n = float(n)
@@ -439,6 +411,11 @@ class PropertyTransformer(Transformer):
         else:
             assert unit == "s"
         return n
+
+    def boolean(self, (b,)):
+        if b == "True":
+            return HplLiteral(b, True)
+        return HplLiteral(b, False)
 
     def string(self, (s,)):
         return HplLiteral(s, s)
