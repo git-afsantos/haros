9dd3b58f963883a39221d6844c89956d1db9525f
André Santos <contact.andre.santos@gmail.com>
2021-08-27 14:08:38 +0100

    v3.10.15

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ extra_files.append("models/*.yaml")
 
 setup(
     name            = "haros",
-    version         = "3.10.14",
+    version         = "3.10.15",
     author          = u"André Santos",
     author_email    = "haros.framework@gmail.com",
     description     = "Static analysis framework for ROS",
